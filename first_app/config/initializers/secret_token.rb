c768c9133b5998585cd46f2eb33ea71cdb607b70
Taras <taras@kowaliw.ca>
2013-06-11 18:51:31 -0400

    Following instructions ar ruby on rails tutorial for rails version 3.2

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '72e9e4d8241e8a14d64b72a6fff51fd1d9017140cd1c98b329910c8eae7b790584127ebc340d3c5ea1eb9cd836c960838a833b2529eb914ab9037f5872df3d08'
